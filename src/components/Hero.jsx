45c588f739a6c155eed99d9ce986bf425719e9ea
BEKLEY Essozolam <essozolam@172-10-51-67.lightspeed.okcbok.sbcglobal.net>
2026-03-05 08:07:35 +0100

    setting infos

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I'm a web developper <br className='sm:block hidden' />
-            i make interfaces and web applications
+            I make interfaces and web applications
           </p>
         </div>
       </div>
